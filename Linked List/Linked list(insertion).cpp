d291785fb317ae058f56eceecbc692f41a4dcb82
harshul_21 <56495746+harshul21@users.noreply.github.com>
2020-07-14 19:31:34 +0530

    Add files via upload

---
+++ Linked List/Linked list(insertion).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node* next;
+
+    ///Constructor
+    node(int d)
+    {
+        data=d;
+        next=NULL;
+    }
+};
+///Passing the pointer variable by reference
+void insertAtHead(node*&head,int data)
+{
+    node*n = new node(data);
+    n->next = head;
+    head = n;
+}
+void print(node* head)
+{
+    node* temp = head;
+
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+}
+int main()
+{
+    node* head=NULL;
+    insertAtHead(head,5);
+    insertAtHead(head,4);
+    insertAtHead(head,3);
+    print(head);
+    return 0;
+}
