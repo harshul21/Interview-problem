003502cf19d97ef102ff94a2af27379f414bf127
harshul_21 <56495746+harshul21@users.noreply.github.com>
2020-07-15 21:09:31 +0530

    Add files via upload

---
+++ Linked List/Linked list(Taking Input).cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node* next;
+
+    ///Constructor
+    node(int d)
+    {
+        data=d;
+        next=NULL;
+    }
+};
+///Passing the pointer variable by reference
+void insertAtHead(node*&head,int data)
+{
+    node*n = new node(data);
+    n->next = head;
+    head = n;
+}
+int length(node*head)
+{
+    int len=0;
+    while(head!=NULL)
+    {
+        head=head->next;
+        len++;
+    }
+    return len;
+}
+void insertAtTail(node*&head,int data)
+{
+    if(head==NULL)
+    {
+        head = new node(data);
+        return;
+    }
+    node*tail = head;
+    while(tail->next!=NULL)
+    {
+        tail=tail->next;
+    }
+    tail->next = new node(data);
+    return;
+}
+void insertAtMiddle(node*&head,int data,int p)
+{
+    if(head==NULL||p==0)
+    {
+        insertAtHead(head,data);
+    }
+    else if(p>length(head))
+    {
+        insertAtTail(head,data);
+    }
+    else
+    {
+        ///Insert in the middle
+        ///take p-1 jumps
+        int jump=1;
+        node*temp = head;
+        while(jump<=p-1)
+        {
+            temp=temp->next;
+            jump++;
+        }
+        node*n = new node(data);
+        n->next = temp->next;
+        temp->next = n;
+
+    }
+}
+bool searchRecursive(node*&head,int key)
+{
+    if(head==NULL)
+    {
+        return false;
+    }
+    ///Rec Case
+    if(head->data==key)
+    {
+        return true;
+    }
+    else
+    {
+        return searchRecursive(head->next,key);
+    }
+}
+bool searchIterative(node*head,int key)
+{
+    while(head!=NULL)
+    {
+        if(head->data==key)
+        {
+            return true;
+        }
+        head=head->next;
+    }
+    return false;
+}
+void print(node* head)
+{
+    node* temp = head;
+
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+}
+void buildList(node*&head)
+{
+    int data;
+    cin>>data;
+    while(data!=-1)
+    {
+        insertAtTail(head,data);
+        cin>>data;
+    }
+}
+int main()
+{
+    node* head=NULL;
+    buildList(head);
+    print(head);
+    return 0;
+}
