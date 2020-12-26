56b57385831eab2f805b74495dd28d88ea9fc751
harshul_21 <56495746+harshul21@users.noreply.github.com>
2020-12-26 16:01:41 +0530

    Add files via upload

---
+++ Bit Manipulation/Count number of ones.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int numberofone(int n)
+{
+    int count =0;
+    while(n)
+    {
+        n=n&n-1;
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    cout<<numberofone(19);
+    return 0;
+}
