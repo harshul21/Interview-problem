aeefcc998659be21119e3f42c249c333c77188b1
harshul_21 <56495746+harshul21@users.noreply.github.com>
2020-12-26 10:48:10 +0530

    Update lower to upper case.cpp

--- String/lower to upper case.cpp
+++ String/lower to upper case.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     string str = "harshul garg";
+    // convert into upper case
     for(int i=0;i<str.length();i++)
     {
         if(str[i]>='a' && str[i]<='z')
@@ -11,5 +12,16 @@ int main()
             str[i]-=32;
         }
     }
+    cout<<str<<endl;
+    
+    //convert intp lower case
+    for(int i=0;i<str.lenght();i++)
+    {
+        if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i]+=32;
+        }
+    }
+    
     cout<<str;
 }
