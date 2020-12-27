135f301f2d179b0a9c16dc9554850b7a13674210
harshul_21 <56495746+harshul21@users.noreply.github.com>
2020-12-27 12:12:34 +0530

    Add files via upload

---
+++ Euclid gcd.cpp
@@ -0,0 +1,18 @@
+//Euclid GCD Algorithm
+#include<iostream>
+using namespace std;
+int gcd(int a,int b)
+{
+    while(b!=0)
+    {
+        int rem=a%b;
+        a=b;
+        b=rem;
+    }
+    return a;
+}
+int main()
+{
+    cout<<gcd(42,24);
+    return 0;
+}
