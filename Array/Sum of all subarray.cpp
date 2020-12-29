e09611c9b66dc031c49bf5be520efcc6d6d92e0f
harshul_21 <56495746+harshul21@users.noreply.github.com>
2020-12-29 12:16:45 +0530

    Create Sum of all subarray.cpp

---
+++ Array/Sum of all subarray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int sum;
+    for(int i=0;i<n;i++)
+    {
+        sum=0;
+        for(int j=i;j<n;j++)
+        {
+            sum=sum+arr[j];
+            cout<<sum<<endl;
+        }
+    }
+}
