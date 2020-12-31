19c9c01ccfc13ecd45210ec21102536707130769
harshul_21 <56495746+harshul21@users.noreply.github.com>
2020-12-31 10:34:49 +0530

    Create Transpose of a matrix.cpp

---
+++ Array/Transpose of a matrix.cpp
@@ -0,0 +1,35 @@
+//seems a easier ques but try wrtitng its code
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(j>i)
+            {
+                swap(arr[i][j],arr[j][i]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
