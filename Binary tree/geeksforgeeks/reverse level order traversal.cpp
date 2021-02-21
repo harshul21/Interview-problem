ed52d6e5162d8a677d947bf804dfb8693b5ff72e
harshul_21 <56495746+harshul21@users.noreply.github.com>
2021-02-21 14:52:45 +0530

    Create reverse level order traversal.cpp

---
+++ Binary tree/geeksforgeeks/reverse level order traversal.cpp
@@ -0,0 +1,29 @@
+//Important-first traverse the right before left
+vector<int> reverseLevelOrder(Node *root)
+{
+    vector<int> v;
+    stack<int> s;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node* temp=q.front();
+        q.pop();
+        s.push(temp->data);
+        if(temp->right)
+        {
+            q.push(temp->right);
+        }
+        if(temp->left)
+        {
+            q.push(temp->left);
+        }
+    }
+    while(!s.empty())
+    {
+        int x=s.top();
+        v.push_back(x);
+        s.pop();
+    }
+    return v;
+}
